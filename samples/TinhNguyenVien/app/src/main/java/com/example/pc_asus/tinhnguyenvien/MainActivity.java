44f878c4ab5a92b92f59f9c3ff08a5b89c38abf6
PC_BSS <hobinhit@gmail.com>
2018-08-31 16:40:32 +0700

    add all file

---
+++ samples/TinhNguyenVien/app/src/main/java/com/example/pc_asus/tinhnguyenvien/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pc_asus.tinhnguyenvien;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, VideoChatViewActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ samples/TinhNguyenVien/app/src/main/java/com/example/pc_asus/tinhnguyenvien/VideoChatViewActivity.java
@@ -0,0 +1,203 @@
+package com.example.pc_asus.tinhnguyenvien;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import io.agora.rtc.Constants;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+
+public class VideoChatViewActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = VideoChatViewActivity.class.getSimpleName();
+
+    private static final int PERMISSION_REQ_ID_RECORD_AUDIO = 22;
+    private static final int PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1;
+
+    private RtcEngine mRtcEngine;// Tutorial Step 1
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() { // Tutorial Step 1
+        @Override
+        public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) { // Tutorial Step 5
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) { // Tutorial Step 7
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onRemoteUserLeft();
+                }
+            });
+        }
+
+        @Override
+        public void onUserMuteVideo(final int uid, final boolean muted) { // Tutorial Step 10
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onRemoteUserVideoMuted(uid, muted);
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat_view);
+
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO) && checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA)) {
+            initAgoraEngineAndJoinChannel();
+        }
+    }
+
+    private void initAgoraEngineAndJoinChannel() {
+        initializeAgoraEngine();     // Tutorial Step 1
+        setupVideoProfile();         // Tutorial Step 2
+        //    setupLocalVideo();           // Tutorial Step 3
+        joinChannel();               // Tutorial Step 4
+        mRtcEngine.switchCamera();
+    }
+
+    public boolean checkSelfPermission(String permission, int requestCode) {
+        Log.i(LOG_TAG, "checkSelfPermission " + permission + " " + requestCode);
+        if (ContextCompat.checkSelfPermission(this,
+                permission)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{permission},
+                    requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        Log.i(LOG_TAG, "onRequestPermissionsResult " + grantResults[0] + " " + requestCode);
+
+        switch (requestCode) {
+            case PERMISSION_REQ_ID_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA);
+                } else {
+                    showLongToast("No permission for " + Manifest.permission.RECORD_AUDIO);
+                    finish();
+                }
+                break;
+            }
+            case PERMISSION_REQ_ID_CAMERA: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initAgoraEngineAndJoinChannel();
+                } else {
+                    showLongToast("No permission for " + Manifest.permission.CAMERA);
+                    finish();
+                }
+                break;
+            }
+        }
+    }
+
+    public final void showLongToast(final String msg) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        leaveChannel();
+        RtcEngine.destroy();
+        mRtcEngine = null;
+    }
+    // Tutorial Step 6
+    public void onEncCallClicked(View view) {
+        finish();
+    }
+
+    // Tutorial Step 1
+    private void initializeAgoraEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e));
+
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    // Tutorial Step 2
+    private void setupVideoProfile() {
+        mRtcEngine.enableVideo();
+        mRtcEngine.setVideoProfile(Constants.VIDEO_PROFILE_480P_10, false);
+    }
+
+    // Tutorial Step 4
+    private void joinChannel() {
+        mRtcEngine.joinChannel(null, "demoChannel1", "Extra Optional Data", 0); // if you do not specify the uid, we will generate the uid for you
+    }
+
+    // Tutorial Step 5
+    private void setupRemoteVideo(int uid) {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+
+        if (container.getChildCount() >= 1) {
+            return;
+        }
+
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(getBaseContext());
+        container.addView(surfaceView);
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_ADAPTIVE, uid));
+
+        surfaceView.setTag(uid); // for mark purpose
+    }
+
+    // Tutorial Step 6
+    private void leaveChannel() {
+        mRtcEngine.leaveChannel();
+    }
+
+    // Tutorial Step 7
+    private void onRemoteUserLeft() {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+        container.removeAllViews();
+    }
+
+    // Tutorial Step 10
+    private void onRemoteUserVideoMuted(int uid, boolean muted) {
+        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+
+        SurfaceView surfaceView = (SurfaceView) container.getChildAt(0);
+
+        Object tag = surfaceView.getTag();
+        if (tag != null && (Integer) tag == uid) {
+            surfaceView.setVisibility(muted ? View.GONE : View.VISIBLE);
+        }
+    }
+}
